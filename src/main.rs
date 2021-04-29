90746ae785155348bcbdd8ca931bd4bd81a922d6
Scott Mabin <scott@mabez.dev>
2021-04-29 15:34:48 +0100

    Renable write protect

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,19 @@ fn main() -> ! {
     // disable wdt's
     unsafe {
         // tg0 wdg
-        core::ptr::write_volatile(0x6001f064 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x6001f064 as *mut _, 0x50D83AA1); // disable write protect
         core::ptr::write_volatile(0x6001F048 as *mut _, 0);
+        core::ptr::write_volatile(0x6001f064 as *mut _, 0); // enable write protect
 
         // tg1 wdg
-        core::ptr::write_volatile(0x60020064 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x60020064 as *mut _, 0x50D83AA1); // disable write protect
         core::ptr::write_volatile(0x60020048 as *mut _, 0);
+        core::ptr::write_volatile(0x60020064 as *mut _, 0); // enable write protect
 
         // rtc wdg
-        core::ptr::write_volatile(0x600080a8 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x600080a8 as *mut _, 0x50D83AA1); // disable write protect
         core::ptr::write_volatile(0x60008090 as *mut _, 0);
+        core::ptr::write_volatile(0x600080a8 as *mut _, 0); // enable write protect
     }
 
     loop {}
