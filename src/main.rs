2eedc664894f87562769c88cd5453fafbd042d83
Scott Mabin <scott@mabez.dev>
2021-04-29 15:13:44 +0100

    Disable watchdogs

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,26 @@ use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    let mut _tmp: u32;
     // disable interrupts, `csrwi        mie,0` throws an exception on the esp32c3
-    unsafe { llvm_asm!("csrrs $0, mstatus, $1": "=r"(_tmp) : "rK"(0x00000008)) };
+    unsafe { 
+        let mut _tmp: u32;
+        llvm_asm!("csrrs $0, mstatus, $1": "=r"(_tmp) : "rK"(0x00000008)) 
+    };
 
-    loop { continue; }
+    // disable wdt's
+    unsafe {
+        // tg0 wdg
+        core::ptr::write_volatile(0x6001f064 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x6001F048 as *mut _, 0);
+
+        // tg1 wdg
+        core::ptr::write_volatile(0x60020064 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x60020048 as *mut _, 0);
+
+        // rtc wdg
+        core::ptr::write_volatile(0x600080a8 as *mut _, 0x50D83AA1);
+        core::ptr::write_volatile(0x60008090 as *mut _, 0);
+    }
+
+    loop {}
 }
