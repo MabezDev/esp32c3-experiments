fd34e280a4aa9cf8d927aff2140bacb348be0181
Scott Mabin <scott@mabez.dev>
2021-04-29 12:10:14 +0100

    patch riscv and disable interrupts via mstatus

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #![no_std]
 #![no_main]
 
+#![feature(llvm_asm)]
+
 use panic_halt as _;
 
 use riscv_rt::entry;
 
 #[entry]
 fn main() -> ! {
+    let mut _tmp: u32;
+    // disable interrupts, `csrwi        mie,0` throws an exception on the esp32c3
+    unsafe { llvm_asm!("csrrs $0, mstatus, $1": "=r"(_tmp) : "rK"(0x00000008)) };
+
     loop { continue; }
 }
