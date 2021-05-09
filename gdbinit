7fab1a90f6e5b5f306aa697961695c58b97b92e1
Scott Mabin <scott@mabez.dev>
2021-05-09 20:11:27 +0100

    Set super wdg to autofeed mode

--- gdbinit
+++ gdbinit
@@ -1,2 +1,4 @@
 target remote :3333
 mon reset halt
+# load
+# break main
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![no_main]
-
 #![feature(llvm_asm)]
 
 use panic_halt as _;
@@ -10,27 +9,32 @@ use riscv_rt::entry;
 #[entry]
 fn main() -> ! {
     // disable interrupts, `csrwi        mie,0` throws an exception on the esp32c3
-    unsafe { 
+    unsafe {
         let mut _tmp: u32;
-        llvm_asm!("csrrs $0, mstatus, $1": "=r"(_tmp) : "rK"(0x00000008)) 
+        llvm_asm!("csrrs $0, mstatus, $1": "=r"(_tmp) : "rK"(0x00000008))
     };
 
     // disable wdt's
     unsafe {
+        // super wdt
+        core::ptr::write_volatile(0x600080B0 as *mut _, 0x8F1D312Au32); // disable write protect
+        core::ptr::write_volatile(0x600080AC as *mut _, core::ptr::read_volatile(0x600080AC as *const u32) | 1 << 31); // set RTC_CNTL_SWD_AUTO_FEED_EN
+        core::ptr::write_volatile(0x600080B0 as *mut _, 0u32); // enable write protect
+
         // tg0 wdg
-        core::ptr::write_volatile(0x6001f064 as *mut _, 0x50D83AA1); // disable write protect
-        core::ptr::write_volatile(0x6001F048 as *mut _, 0);
-        core::ptr::write_volatile(0x6001f064 as *mut _, 0); // enable write protect
+        core::ptr::write_volatile(0x6001f064 as *mut _, 0x50D83AA1u32); // disable write protect
+        core::ptr::write_volatile(0x6001F048 as *mut _, 0u32);
+        core::ptr::write_volatile(0x6001f064 as *mut _, 0u32); // enable write protect
 
         // tg1 wdg
-        core::ptr::write_volatile(0x60020064 as *mut _, 0x50D83AA1); // disable write protect
-        core::ptr::write_volatile(0x60020048 as *mut _, 0);
-        core::ptr::write_volatile(0x60020064 as *mut _, 0); // enable write protect
+        core::ptr::write_volatile(0x60020064 as *mut _, 0x50D83AA1u32); // disable write protect
+        core::ptr::write_volatile(0x60020048 as *mut _, 0u32);
+        core::ptr::write_volatile(0x60020064 as *mut _, 0u32); // enable write protect
 
         // rtc wdg
-        core::ptr::write_volatile(0x600080a8 as *mut _, 0x50D83AA1); // disable write protect
-        core::ptr::write_volatile(0x60008090 as *mut _, 0);
-        core::ptr::write_volatile(0x600080a8 as *mut _, 0); // enable write protect
+        core::ptr::write_volatile(0x600080a8 as *mut _, 0x50D83AA1u32); // disable write protect
+        core::ptr::write_volatile(0x60008090 as *mut _, 0u32);
+        core::ptr::write_volatile(0x600080a8 as *mut _, 0u32); // enable write protect
     }
 
     loop {}
